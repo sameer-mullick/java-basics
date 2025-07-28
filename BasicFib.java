6b7e24e02434a2344b2fc4c0bc3b24dbd4e03a56
Mullick12 <sameer.n.mullick@gmail.com>
2025-07-28 00:10:16 -0400

    Initial commit

---
+++ BasicFib.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+class BasicFib {
+    public static void main(String args[]) {
+        Scanner scan = new Scanner(System.in);
+        int n;
+
+        while (true) {
+            System.out.print("Enter a positive number (between 2 and 20): ");
+            n = scan.nextInt();
+
+            if (n >= 2 && n <= 20) {
+                break;
+            } else {
+                System.out.println("Invalid input. Please try again.");
+            }
+        }
+
+        int fib[] = new int[n];
+        fib[0] = 0;
+        fib[1] = 1;
+
+        System.out.println("First " + n + " terms of the Fibonacci sequence:");
+
+        int count = 2;
+        while (count < n) {
+            fib[count] = fib[count - 2] + fib[count - 1];
+            count++;
+        }
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(fib[i] + " ");
+        }
+
+        System.out.println("\n*** Finished! ***");
+    }
+}
